0f7ea7fb11c425cdcae7d50239e364bc345fd243
sidawspip <siddharthkushwah3@gmail.com>
2023-09-28 13:24:10 +0530

    updated build

--- src/main/java/com/github/actions/controller/UserController.java
+++ src/main/java/com/github/actions/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @GetMapping("/")
+    @GetMapping
     public String msg(){
         return "Hello,Github Actions is working!!!";
     }
